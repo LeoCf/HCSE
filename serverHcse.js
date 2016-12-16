f8b97c6655533ce57b2392399f432167856a1d0e
JPedroBorges <joao.pedro.borges@hotmail.com>
2016-12-16 17:13:55 +0000

    MySQL connection

--- serverHcse.js
+++ serverHcse.js
@@ -3,10 +3,21 @@
 //Dependencias
 var express    = require('express');        // dependencia do express
 var app        = express();                 // define our app using express
-var bodyParser = require('body-parser');    // para lidar com Json , 
+var bodyParser = require('body-parser');    // para lidar com Json ,
 var request = require("request");
+var mysql      = require('mysql');			// ligação ao mysql
 
-//Ligação ao Vera 
+//Base de dados
+//https://github.com/mysqljs/mysql
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'me',
+  password : 'secret',
+  database : 'my_db'
+});
+connection.connect();
+
+//Ligação ao Vera
 var ligaçoes = ['http://aveiro.m-iti.org/hybridnilm/public/api/v1/plugwise/samples/hourly/plug/000D6F000261B01C/2016-11-26','http://192.168.2.121:3480/data_request?id=sdata&output_format=json'];
 var resultados = [];
 var deviceUrl1 = 'http://192.168.2.121:3480/data_request?id=action&output_format=xml&DeviceNum=';
@@ -23,22 +34,22 @@ de um aparelho especificado pelo o mac adress para esse dia todo
 function ligaçãoRecursiva (urls)
  {
   var url = urls.pop();                               //pop manda para fora o ultimo elemento do array e retorna o elemento removido
-  request(url,function(error,response,body){		
-      resultados.push(body);		
-   	} );						
+  request(url,function(error,response,body){
+      resultados.push(body);
+   	} );
       if(urls.length)
       {
         ligaçãoRecursiva(urls);
-      } 
+      }
       else
       {
       	return resultados;
       	console.log("Recolha de Dados Completa");
-      } 
+      }
  }
 
 console.log(ligaçãoRecursiva(ligaçoes));
-//conectar a box da casa do lucas 
+//conectar a box da casa do lucas
 /*
 var conectarBox = {
   url: 'http://aveiro.m-iti.org/hybridnilm/public/api/v1/plugwise/samples/hourly/plug/'
@@ -49,14 +60,14 @@ var conectarBox = {
 request(conectarBox, function (error, response, body) {
   if (error) throw new Error(error);
 
-  
 
-  
+
+
   console.log(body);
 });
 
 
-// parametros para estabelecer a ligação com a caixa 
+// parametros para estabelecer a ligação com a caixa
 var conectarVera = {
   url: 'http://192.168.2.121:3480/data_request',
   qs: { id: 'sdata', output_format: 'json' }
@@ -65,7 +76,7 @@ var conectarVera = {
 request(conectarVera, function (error, response, body) {
   if (error) throw new Error(error);
 
-  
+
 
   vera=body;
   console.log(vera);
@@ -79,27 +90,27 @@ var router = express.Router();
 
 
 router.get('/', function(req, res) {
-    res.json({ message: 'Bem vindo aos web services!' });   
+    res.json({ message: 'Bem vindo aos web services!' });
 });
 
 
-//Registar as routas 
+//Registar as routas
 
 app.use('/',router);
 
 
-//Retorna os dados Json da casa do lucas e dos dispositivos ligados ao vera 
+//Retorna os dados Json da casa do lucas e dos dispositivos ligados ao vera
 app.get('/RawData',function(req,res){
 	res.json(resultados);
 	console.log("Pedido de RawData Recebido");
 });
 
 
-//Liga ou Desliga  status 1 ou 0 , um dispositivo especificado pelo id 
+//Liga ou Desliga  status 1 ou 0 , um dispositivo especificado pelo id
 app.post('/ToggleDevice/:device_id/:status',function(req,res,next)
 {
 	var device_id = req.param('device_id');
-    var status = req.param('status');  
+    var status = req.param('status');
     var url=deviceUrl1+device_id+deviceUrl2+status;
     request(url,function(error,response,body)
     {
@@ -112,6 +123,6 @@ app.post('/ToggleDevice/:device_id/:status',function(req,res,next)
 
 
 
-//Inciar o servidor 
+//Inciar o servidor
 app.listen(port);
 console.log("Servidor iniciado na porta 8080");
